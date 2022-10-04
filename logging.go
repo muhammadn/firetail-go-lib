dbe5728f74e64b522f7f43973bbb9b6d61054bf2
Joshua O'Sullivan <joshua.r.osullivan@gmail.com>
2022-10-04 15:40:40 +0100

    Strip down logging request body

--- logging.go
+++ logging.go
@@ -15,23 +15,16 @@ type RequestPayload struct {
 	HttpProtocol string              `json:"httpProtocol"`
 	Url          string              `json:"url"`
 	Headers      map[string][]string `json:"headers"` // TODO: ensure type matches python lib
-	Path         string              `json:"path"`
 	Method       string              `json:"method"`
-	OPath        string              `json:"oPath"`
-	FPath        string              `json:"fPath"`
-	Args         map[string][]string `json:"args"` // TODO: ensure type matches python lib
 	Body         string              `json:"body"`
 	Ip           string              `json:"ip"`
-	PathParams   string              `json:"pathParams"`
 }
 
 // The responsePayload struct holds the information about the response payload that is passed to the firetail logging endpoint
 type ResponsePayload struct {
-	Status_code     int                 `json:"status_code"`
-	Content_len     int                 `json:"content_len"`
-	Content_enc     string              `json:"content_enc"`
-	Failed_res_body string              `json:"failed_res_body"`
-	Body            string              `json:"body"`
-	Headers         map[string][]string `json:"headers"` // TODO: ensure type matches python lib
-	Content_type    string              `json:"content_type"`
+	Status_code int                 `json:"status_code"`
+	Content_len int                 `json:"content_len"`
+	Content_enc string              `json:"content_enc"`
+	Body        string              `json:"body"`
+	Headers     map[string][]string `json:"headers"` // TODO: ensure type matches python lib
 }
