0ffdd943f68544375ee84d4b6605b2aa29b19470
Joshua O'Sullivan <joshua.r.osullivan@gmail.com>
2022-10-04 15:53:11 +0100

    Doc logged values in example endpoint

--- examples/openapi3/main.go
+++ examples/openapi3/main.go
@@ -11,10 +11,17 @@ import (
 func health(w http.ResponseWriter, r *http.Request) {
 	res := "I'm Healthy, and I take some time!"
 	log.Println(res)
+
 	// Firetail will log the execution time, let's pretend this endpoint takes about 50ms...
 	time.Sleep(50 * time.Millisecond)
+
+	// Firetail will also log the response code...
 	w.WriteHeader(200)
+
+	// Firetail will also capture response headers...
 	w.Header().Add("example-header", "example-value")
+
+	// And, finally, it'll also log the response body...
 	w.Write([]byte(res))
 }
 
