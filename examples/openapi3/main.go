6f2b161ff7549fa15d5956576190f49929c650f4
Joshua O'Sullivan <joshua.r.osullivan@gmail.com>
2022-10-04 15:52:48 +0100

    Artificially increase execution time for example app

--- examples/openapi3/main.go
+++ examples/openapi3/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/FireTail-io/firetail-go-lib"
 )
 
 func health(w http.ResponseWriter, r *http.Request) {
-	res := "I'm Healthy!"
+	res := "I'm Healthy, and I take some time!"
 	log.Println(res)
+	// Firetail will log the execution time, let's pretend this endpoint takes about 50ms...
+	time.Sleep(50 * time.Millisecond)
 	w.WriteHeader(200)
 	w.Header().Add("example-header", "example-value")
 	w.Write([]byte(res))
