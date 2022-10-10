33c0c865f9d726d76c193cec69b27164cdd6876e
Joshua O'Sullivan <joshua.r.osullivan@gmail.com>
2022-10-10 12:20:57 +0200

    Initial MaskHeaders implementation

---
+++ utils/mask_headers.go
@@ -0,0 +1,97 @@
+package utils
+
+import (
+	"crypto"
+	"fmt"
+)
+
+type HeaderMask int
+
+const (
+	// If the mask is being applied strictly, then the header will be removed, otherwise it will be preserved
+	Unset HeaderMask = iota
+
+	// The header will be preserved and logged to FireTail as received
+	PreserveHeader
+
+	// The header will be removed entirely and not reported to Firetail
+	RemoveHeader
+
+	// The header value will be removed and not reported to FireTail
+	RemoveHeaderValues
+
+	// The header's value will hashed before it is reported to Firetail
+	HashHeaderValues
+
+	// Both the header's name and value will be hashed before being reported to Firetail
+	HashHeader
+)
+
+func MaskHeaders(unmaskedHeaders map[string][]string, headersMask map[string]HeaderMask, isStrict bool) map[string][]string {
+	maskedHeaders := map[string][]string{}
+
+	headerNameHashFails := 0
+	for headerName, headerValues := range unmaskedHeaders {
+		mask := headersMask[headerName]
+
+		switch mask {
+		case Unset:
+			// If the mask is being applied strictly, and the headersMask is Unset for this header, we skip it
+			if isStrict {
+				continue
+			}
+			// Else, we treat it as if it's preserved
+			maskedHeaders[headerName] = headerValues
+			break
+
+		case PreserveHeader:
+			maskedHeaders[headerName] = headerValues
+			break
+
+		case RemoveHeader:
+			// Nothing to do here!
+			break
+
+		case RemoveHeaderValues:
+			maskedHeaders[headerName] = []string{}
+			break
+
+		case HashHeaderValues:
+			maskedHeaders[headerName] = hashValues(headerValues)
+			break
+
+		case HashHeader:
+			hashedHeaderName, err := hashValue(headerName)
+			if err != nil {
+				hashedHeaderName = fmt.Sprintf("Failed to hash header name (%d)", headerNameHashFails)
+				headerNameHashFails += 1
+			}
+			maskedHeaders[hashedHeaderName] = hashValues(headerValues)
+			break
+		}
+	}
+
+	return maskedHeaders
+}
+
+func hashValues(values []string) []string {
+	hashedValues := []string{}
+	for _, value := range values {
+		hashedValue, err := hashValue(value)
+		if err != nil {
+			hashedValues = append(hashedValues, "Failed to hash, err: "+err.Error())
+			continue
+		}
+		hashedValues = append(hashedValues, hashedValue)
+	}
+	return hashedValues
+}
+
+func hashValue(value string) (string, error) {
+	hasher := crypto.SHA1.New()
+	_, err := hasher.Write([]byte(value))
+	if err != nil {
+		return "", err
+	}
+	return string(hasher.Sum(nil)), nil
+}
