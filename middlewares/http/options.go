b7332e621129cd3ceaff69129c600688949682f5
Joshua O'Sullivan <joshua.r.osullivan@gmail.com>
2022-10-10 15:40:41 +0200

    Add request & response sanitisation callbacks to middleware options

--- middlewares/http/options.go
+++ middlewares/http/options.go
@@ -2,6 +2,7 @@ package firetail
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/FireTail-io/firetail-go-lib/headers"
 	"github.com/FireTail-io/firetail-go-lib/utils"
@@ -25,11 +26,21 @@ type Options struct {
 	// DisableValidation is an optional flag which, if set to true, disables request & response validation
 	DisableValidation bool
 
+	// RequestSanitisationCallback is an optional callback which is given the request body as bytes & returns a stringified request body which
+	// is then logged to Firetail. This is useful for writing custom logic to redact any sensitive data from your request bodies before it is logged
+	// in Firetail.
+	RequestSanitisationCallback func([]byte) string
+
 	// RequestHeadersMask is a map of header names to HeaderMask values, which can be used to control the request headers reported to Firetail
 	RequestHeadersMask *map[string]headers.HeaderMask
 
 	// RequestHeadersMaskStrict is an optional flag which, if set to true, will configure the Firetail middleware to only report request headers explicitly described in the RequestHeadersMask
 	RequestHeadersMaskStrict bool
+
+	// ResponseSanitisationCallback is an optional callback which is given the response body as bytes & returns a stringified response body which
+	// is then logged to Firetail. This is useful for writing custom logic to redact any sensitive data from your response bodies before it is logged
+	// in Firetail.
+	ResponseSanitisationCallback func([]byte) string
 
 	// ResponseHeadersMask is a map of header names to HeaderMask values, which can be used to control the response headers reported to Firetail
 	ResponseHeadersMask *map[string]headers.HeaderMask
@@ -43,15 +54,58 @@ type Options struct {
 
 func (o *Options) setDefaults() {
 	if o.SourceIPCallback == nil {
-		o.SourceIPCallback = defaultSourceIPCallback
+		o.SourceIPCallback = func(r *http.Request) string {
+			return strings.Split(r.RemoteAddr, ":")[0]
+		}
 	}
+
 	if o.ErrHandler == nil {
-		o.ErrHandler = defaultErrHandler
+		o.ErrHandler = func(err error, w *utils.ResponseWriter) {
+			switch err {
+			case utils.ErrPathNotFound:
+				w.WriteHeader(404)
+				w.Write([]byte("404 - Not Found"))
+				break
+			case utils.ErrMethodNotAllowed:
+				w.WriteHeader(405)
+				w.Write([]byte("405 - Method Not Allowed"))
+				break
+			case utils.ErrRequestValidationFailed:
+				w.WriteHeader(400)
+				w.Write([]byte("400 - Method Not Allowed"))
+				break
+			case utils.ErrResponseValidationFailed:
+				w.WriteHeader(500)
+				w.Write([]byte("500 - Internal Server Error"))
+				return
+			default:
+				// Even if the err is nil, we return a 500, as defaultErrHandler should never be called with a nil err
+				w.WriteHeader(500)
+				w.Write([]byte("500 - Internal Server Error"))
+				break
+			}
+		}
 	}
+
+	if o.RequestSanitisationCallback == nil {
+		o.RequestSanitisationCallback = func(b []byte) string {
+			return string(b)
+		}
+	}
+
 	if o.RequestHeadersMask == nil {
-		o.RequestHeadersMask = getDefaultResponseHeadersMask()
+		// TODO: create default
+		o.RequestHeadersMask = &map[string]headers.HeaderMask{}
 	}
+
+	if o.ResponseSanitisationCallback == nil {
+		o.ResponseSanitisationCallback = func(b []byte) string {
+			return string(b)
+		}
+	}
+
 	if o.ResponseHeadersMask == nil {
-		o.RequestHeadersMask = getDefaultResponseHeadersMask()
+		// TODO: create default
+		o.RequestHeadersMask = &map[string]headers.HeaderMask{}
 	}
 }
