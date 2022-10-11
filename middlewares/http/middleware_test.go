30bbc112ab4b4e74437a505b0f0f38fd820496c4
Joshua O'Sullivan <joshua.r.osullivan@gmail.com>
2022-10-11 10:11:03 +0200

    Test 405

--- middlewares/http/middleware_test.go
+++ middlewares/http/middleware_test.go
@@ -94,3 +94,35 @@ func TestRequestToInvalidRoute(t *testing.T) {
 	require.Nil(t, err)
 	assert.Equal(t, "404 - Not Found", string(respBody))
 }
+
+func TestRequestWithDisallowedMethod(t *testing.T) {
+	// Get a middleware
+	middleware, err := GetMiddleware(&Options{
+		SpecPath: "./test-spec.yaml",
+	})
+	require.Nil(t, err)
+
+	// Create our handler
+	handler := middleware(healthHandler)
+
+	// Create our response recorder & test request
+	responseRecorder := httptest.NewRecorder()
+	request := httptest.NewRequest("POST", "/health", nil)
+
+	// Handle the test request & record the response
+	handler.ServeHTTP(responseRecorder, request)
+
+	// Status code should be 200
+	assert.Equal(t, 405, responseRecorder.Code)
+
+	// Should have a Content-Type: text/plain header
+	require.Contains(t, responseRecorder.HeaderMap, "Content-Type")
+	require.GreaterOrEqual(t, len(responseRecorder.HeaderMap["Content-Type"]), 1)
+	assert.Len(t, responseRecorder.HeaderMap["Content-Type"], 1)
+	assert.Equal(t, responseRecorder.HeaderMap["Content-Type"][0], "text/plain")
+
+	// Response body should be "I'm Healthy! :)"
+	respBody, err := io.ReadAll(responseRecorder.Body)
+	require.Nil(t, err)
+	assert.Equal(t, "405 - Method Not Allowed", string(respBody))
+}
