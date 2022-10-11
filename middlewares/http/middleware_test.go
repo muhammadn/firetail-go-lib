e82b9f450a934ba6c213e6b8ac57718631206a50
Joshua O'Sullivan <joshua.r.osullivan@gmail.com>
2022-10-11 10:02:57 +0200

    Test request to invalid route

--- middlewares/http/middleware_test.go
+++ middlewares/http/middleware_test.go
@@ -62,3 +62,35 @@ func TestInvalidSpec(t *testing.T) {
 	})
 	require.Equal(t, "invalid paths: a short description of the response is required", err.Error())
 }
+
+func TestRequestToInvalidRoute(t *testing.T) {
+	// Get a middleware
+	middleware, err := GetMiddleware(&Options{
+		SpecPath: "./test-spec.yaml",
+	})
+	require.Nil(t, err)
+
+	// Create our handler
+	handler := middleware(healthHandler)
+
+	// Create our response recorder & test request
+	responseRecorder := httptest.NewRecorder()
+	request := httptest.NewRequest("GET", "/not-here", nil)
+
+	// Handle the test request & record the response
+	handler.ServeHTTP(responseRecorder, request)
+
+	// Status code should be 200
+	assert.Equal(t, 404, responseRecorder.Code)
+
+	// Should have a Content-Type: text/plain header
+	require.Contains(t, responseRecorder.HeaderMap, "Content-Type")
+	require.GreaterOrEqual(t, len(responseRecorder.HeaderMap["Content-Type"]), 1)
+	assert.Len(t, responseRecorder.HeaderMap["Content-Type"], 1)
+	assert.Equal(t, responseRecorder.HeaderMap["Content-Type"][0], "text/plain")
+
+	// Response body should be "I'm Healthy! :)"
+	respBody, err := io.ReadAll(responseRecorder.Body)
+	require.Nil(t, err)
+	assert.Equal(t, "404 - Not Found", string(respBody))
+}
