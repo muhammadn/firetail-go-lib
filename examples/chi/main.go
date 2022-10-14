a2174c38f55147f0d612e84937eb1a4ca30bb630
Joshua O'Sullivan <joshua.r.osullivan@gmail.com>
2022-10-14 14:22:52 +0200

    Install firetail middleware

--- examples/chi/main.go
+++ examples/chi/main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"net/http"
 
+	firetail "github.com/FireTail-io/firetail-go-lib/middlewares/http"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	firetailMiddleware, err := firetail.GetMiddleware(&firetail.Options{
+		SpecPath: "app-spec.yaml",
+	})
+	if err != nil {
+		panic(err)
+	}
+
 	r := chi.NewRouter()
+	r.Use(firetailMiddleware)
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
