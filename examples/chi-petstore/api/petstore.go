b105486a3b87462da0cfded83308e21787c06ddb
Joshua O'Sullivan <joshua.r.osullivan@gmail.com>
2022-10-24 17:29:42 +0100

    Add missing content-type header to GET /pets

--- examples/chi-petstore/api/petstore.go
+++ examples/chi-petstore/api/petstore.go
@@ -66,6 +66,7 @@ func (p *PetStore) FindPets(w http.ResponseWriter, r *http.Request, params FindP
 		}
 	}
 
+	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(result)
 }
