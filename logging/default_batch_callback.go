dcf7bf1dadc50a96e4a1063c36d0f064eea9c42e
Joshua O'Sullivan <joshua.r.osullivan@gmail.com>
2022-10-28 15:39:25 +0100

    Setup retries in default log batch callback

--- logging/default_batch_callback.go
+++ logging/default_batch_callback.go
@@ -5,48 +5,53 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"log"
 	"net/http"
 )
 
 func getDefaultBatchCallback(options BatchLoggerOptions) func([][]byte) {
 	sendBatch := func(batchBytes [][]byte) error {
-		if options.LogApiUrl == "" {
-			return errors.New("no log api url set")
-		}
 		reqBytes := []byte{}
 		for _, entry := range batchBytes {
 			reqBytes = append(reqBytes, entry...)
 			reqBytes = append(reqBytes, '\n')
 		}
+
 		req, err := http.NewRequest("POST", options.LogApiUrl, bytes.NewBuffer(reqBytes))
 		if err != nil {
 			return err
 		}
+
 		req.Header.Set("x-ft-api-key", options.LogApiKey)
+
 		resp, err := http.DefaultClient.Do(req)
 		if err != nil {
 			return err
 		}
+
 		var res map[string]interface{}
 		json.NewDecoder(resp.Body).Decode(&res)
 		if res["message"] != "success" {
 			return errors.New(fmt.Sprintf("got err response from firetail api: %v", res))
 		}
+
 		return nil
 	}
 
 	return func(batch [][]byte) {
-		payload := ""
-		for _, entry := range batch {
-			payload += string(entry) + "\r\n"
+		// If there's no log API url or log API key set then we can't log, so just return
+		if options.LogApiUrl == "" || options.LogApiKey == "" {
+			return
 		}
-		log.Printf("Sending batch of len %d to Firetail. Payload:\n%s", len(batch), payload)
 
-		err := sendBatch(batch)
-
-		if err != nil {
-			log.Printf("Err calling batchcallback: %s", err.Error())
+		var err error
+		retries := 0
+		for {
+			err = sendBatch(batch)
+			retries++
+			// If sendBatch succeeded, or we've had 3 retries, we give up
+			if err == nil || retries >= 3 {
+				break
+			}
 		}
 	}
 }
